b938ef4e32b7a9202adc0e9cfe21a98a1e0ac7a7
sp0xff <sp0xff@gmail.com>
2020-02-01 10:13:24 +0500

    Updates

---
+++ precache-manifest.cb92e0b50596c4a4291951d34a5880e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71741367000a7c7e28240d62cd637b3d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "62a7a45028174c376c25",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "9a685a8ac4bf951048e1",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "62a7a45028174c376c25",
+    "url": "./static/js/2.740ca6d1.chunk.js"
+  },
+  {
+    "revision": "9a685a8ac4bf951048e1",
+    "url": "./static/js/main.805a2ab8.chunk.js"
+  },
+  {
+    "revision": "874b899ddd063884ffe1",
+    "url": "./static/js/runtime-main.a40c4999.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
